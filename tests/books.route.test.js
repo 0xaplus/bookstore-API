0ecf6331c55602461ce833c1cd8257eb5eb16aff
0xaplus <codewithaplus@gmail.com>
2023-12-08 23:16:55 +0100

    New endpoints tests

--- tests/books.route.test.js
+++ tests/books.route.test.js
@@ -1,11 +1,27 @@
 const supertest = require("supertest");
 const booksRoute = require("../books");
+const server = require("../index");
 
-describe('Book Route', () => {
-
+describe('Book functions', () => {
     it("GET /books works", async () => {
-        const response = await supertest(booksRoute).get("/books");
-        expect(response.headers["content-type"]).toBe("application/json");
-        expect(response.status).toBe(200);
+        const response = await supertest(server).get("/books")
+        expect(response.headers["content-type"]).toBe("application/json")
+        expect(response.status).toBe(200)
+        expect(response.body.length).toBe(4)
+
+    });
+
+    it("POST /newbook works", async () => {
+        const bookToAdd = {
+            "title": "New test book",
+            "author": "Rising Odegua",
+            "year": 2022,
+        };
+        const response = await supertest(server).post("/newbook").send(bookToAdd)
+        expect(response.headers["content-type"]).toBe("application/json")
+        expect(response.status).toBe(201)
+        expect(response.body.title).toBe("New test book")
+        expect(response.body.author).toBe("Rising Odegua")
+        expect(response.body.year).toBe(2022)
     });
 });
